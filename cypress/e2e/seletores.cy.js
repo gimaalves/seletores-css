f429756d6755cfcbf8254132c0223e55c0b1b0d2
Alves <gisela.alves@capgemini.com>
2024-06-03 15:33:10 -0300

    aprendendo seletores

--- cypress/e2e/seletores.cy.js
+++ cypress/e2e/seletores.cy.js
@@ -7,51 +7,61 @@ describe('Seletores avançados com cypress', () => {
   });
 
   it('Seleciona elementos que contêm um Texto específico', () => {
-    //TODO: 
+    cy.contains('Enviar').click() 
   });
     
   it('Seleciona o elemento com a classe pai', () => {
-    //TODO: 
+    cy.get('.pai').should('exist')
   })
 
   it('Seleciona o elemento com o id Filho', () => {
-    //TODO: 
+    cy.get('#id-filho').should('have.attr', 'class', 'filho-4')
    })
 
   it('Seleciona um elemento filho dentro do elemento com a classe pai', () => {
-    //TODO: 
+    cy.get('.pai').find('.filho-4').should('contain', 'Item solto') 
   });
 
   it('Seleciona o segundo elemento <span> com a classe irmao', () => {
-    //TODO: 
+    cy.get('.irmao + .irmao').should('contain', 'Irmão 2') 
   });
 
   it('Seleciona o próximo elemento irmão', () => {
-    //TODO: 
+    cy.get('.item-especial').next().should('contain', 'Item 3')
   });
 
   it('Seleciona o elemento irmão anterior', () => {
-    //TODO: 
+    cy.get('.item-especial').prev().should('contain', 'Item 1')
   });
 
   it('Seleciona o irmão da div anterior', () => {
-    //TODO: 
+    cy.get('[name="nome-do-atributo"]').prev().should('contain', 'Item 3') 
   });
 
+  //inicia em 0
   it('Seleciona o terceiro elemento <li> encontrado', () => {
-    //TODO: 
+    cy.get('li').eq(2).should('contain', 'Item 3')
   });
 
   it('Seleciona o elemento com o atributo data-test', () => {
-    //TODO: 
+    cy.get('[data-test="div-pai"]').should('contain', 'Item solto')
   });
 
   it('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
-    //TODO: 
+    //cy.get('.item-especial').parent() 
+    cy.get('#id-filho').parent('.pai').should('have.attr', 'data-test', 'div-pai')
   });
 
   it('Seleciona o elemento com um valor em um select', () => {
-    //TODO: 
+    cy.get('select').select('Pouco')
+    cy.get('[type="submit"]').click()
+    cy.get('#mensagemFeedback').should('contain', 'Obrigado por compartilhar conosco!')
+    cy.contains('Obrigado por compartilhar conosco!').should('exist')
+  });
+
+  it('Seleciona um elemento com checkbox', () => {
+    cy.get('#aceitar-termos').check()
+    
   });
 
 })
